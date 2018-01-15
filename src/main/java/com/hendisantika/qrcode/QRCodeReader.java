8d22ea1ee84a8072ef35c3e3d65391c4ba169585
Hendi Santika <hendisantika@gmail.com>
2018-01-16 06:22:17 +0700

    #3 - Add QR Code reader

---
+++ src/main/java/com/hendisantika/qrcode/QRCodeReader.java
@@ -0,0 +1,50 @@
+package com.hendisantika.qrcode;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : qr-code-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/01/18
+ * Time: 06.21
+ * To change this template use File | Settings | File Templates.
+ */
+public class QRCodeReader {
+    private static String decodeQRCode(File qrCodeimage) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("./MyQRCode.png");
+            String decodedText = decodeQRCode(file);
+            if (decodedText == null) {
+                System.out.println("No QR Code found in the image");
+            } else {
+                System.out.println("Decoded text = " + decodedText);
+            }
+        } catch (IOException e) {
+            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
